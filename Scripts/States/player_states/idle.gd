fb4e2f1bc3ea0d6d13bd0be6fc8759fcbdb0d5b4
BendenizMustafaCengiz <mustafacengiz5116@gmail.com>
2026-02-16 16:33:23 +0300

    started player controllers

---
+++ Scripts/States/player_states/idle.gd
@@ -0,0 +1,16 @@
+extends State
+class_name PlayerIdle
+
+@export var player: Player
+@export var animation_player: AnimationPlayer
+
+func enter() -> void:
+	animation_player.play("idle")
+
+
+func update(_delta : float) -> void:
+	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+		transitioned.emit(self, "run")
+
+func physics_update(delta : float) -> void:
+	player.move(Vector2.ZERO, delta)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,1 +1,24 @@
 extends CharacterBody2D
+class_name Player
+
+const MAX_SPEED : int = 600
+var speed_multiplier : float = 1.0
+const ACCEL: float = 40.0
+const FRIC : float = 50.0
+var speed : Vector2 = Vector2.ZERO
+
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+	
+
+func move(dir : Vector2, delta: float)-> void:
+	if dir.length() >0:
+		speed += ACCEL*dir
+	else:
+		speed -= FRIC *speed.normalized()
+	if speed.length() > MAX_SPEED:
+		speed = MAX_SPEED * speed.normalized()
+	elif speed.length() < 0:
+		speed = Vector2.ZERO
+	position += speed*delta
