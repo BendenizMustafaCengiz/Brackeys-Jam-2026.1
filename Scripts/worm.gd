731658b0723a9a0fde9c00f4655a8fc9f166d8b0
Muhammed Cengiz <muhammedcengiz5116@gmail.com>
2026-02-18 20:20:12 +0300

    small changes

--- Scripts/worm.gd
+++ Scripts/worm.gd
@@ -11,6 +11,7 @@ var exited_attack_range = false
 @onready var wait_timer: Timer = $WaitTimer
 @onready var wonder_timer: Timer = $WonderTimer
 @onready var attack_timer: Timer = $AttackTimer
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	init_stats(Save.rooms_cleared)
@@ -26,10 +27,18 @@ func _physics_process(delta: float) -> void:
 	#movement
 	if current_state == state.WONDER or current_state == state.WAIT:
 		position += wonder_speed * wonder_velocity * delta
+		if wonder_velocity.x > 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
 	elif current_state == state.CHASE:
 		var direction = player.position - self.position
 		direction = direction.normalized()
 		position += speed * direction * delta
+		if direction.x > 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
 	
 	move_and_slide()
 
@@ -39,6 +48,8 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 		current_state = state.CHASE
 		wait_timer.stop()
 		wonder_timer.stop()
+		
+		animation_player.play("Move")
 
 func _on_wait_timer_timeout() -> void:
 	wonder_timer.start()
@@ -48,11 +59,15 @@ func _on_wait_timer_timeout() -> void:
 	wonder_velocity.x = randf_range(-1.0, 1.0)
 	wonder_velocity.y = randf_range(-1.0, 1.0)
 	wonder_velocity = wonder_velocity.normalized()
+	
+	animation_player.play("Move")
 
 func _on_wonder_timer_timeout() -> void:
 	wait_timer.start()
 	current_state = state.WAIT
 	wonder_velocity = Vector2(0,0)
+	
+	animation_player.play("Idle")
 
 func _on_attacking_area_body_entered(body: Node2D) -> void:
 	if body is Player and can_attack:
