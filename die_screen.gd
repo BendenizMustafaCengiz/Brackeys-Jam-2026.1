ddb76ab087d6bb6ae9b723631e29783e56c3b745
Muhammed Cengiz <muhammedcengiz5116@gmail.com>
2026-02-19 12:46:38 +0300

    die screen kapılar logicsiz

---
+++ die_screen.gd
@@ -0,0 +1,23 @@
+extends Node2D
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
+
+@onready var game_over_text: RichTextLabel = $CanvasLayer/GameOverText
+@onready var bg: TextureRect = $CanvasLayer/BG
+
+func activate() ->void:
+	canvas_layer.visible = true
+	var tween = create_tween()
+	tween.tween_property(bg,"modulate", Color(0.0, 0.0, 0.0, 0.588), 2.0)
+
+func _on_try_again_button_pressed() -> void:
+	#Zorluk ve map reseti vs vs.
+	ChangeScene.change_scene("res://Scenes/room_scene.tscn")
+
+
+func _on_main_menu_button_pressed() -> void:
+	#Zorluk ve map reseti vs vs.
+	ChangeScene.change_scene("res://Scenes/main_menu.tscn")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("down"):
+		activate()
