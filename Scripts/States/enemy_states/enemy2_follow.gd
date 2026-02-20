8795301dfe576bcce4605c98cab53c081ecbedfa
BendenizMustafaCengiz <mustafacengiz5116@gmail.com>
2026-02-20 12:52:22 +0300

    kılıç bugu düzeltildi dash atan düşman eklendi

---
+++ Scripts/States/enemy_states/enemy2_follow.gd
@@ -0,0 +1,27 @@
+extends State
+class_name Enemy2Follow
+
+
+@export var follow_timer: Timer
+@export var enemy: Enemy
+@export var animation_player: AnimationPlayer
+var player: Player
+
+
+
+func enter() -> void:
+	follow_timer.start(enemy.follow_time)
+	animation_player.play("follow")
+	if ! player:
+		player = get_tree().get_first_node_in_group("player")
+
+func physics_update(delta : float) -> void:
+	var dir: Vector2 = (player.global_position - enemy.global_position).normalized()
+	enemy.position += enemy.speed * dir *delta
+
+
+func _on_follow_time_timeout() -> void:
+	if (player.global_position- enemy.global_position).length() > enemy.dash_limit:
+		transitioned.emit(self,"wait")
+	else:
+		transitioned.emit(self,"dash")
--- Scripts/States/player_states/attack_3.gd
+++ Scripts/States/player_states/attack_3.gd
@@ -8,8 +8,8 @@ class_name PlayerAttack3
 @export var combo_transition_timer: Timer
 var is_dashing: bool = true
 var dash_dir: Vector2
+
 func enter() -> void:
-	animation_player.play("dash")
 	dash_dir = Vector2.RIGHT.rotated(sword.rotation).normalized()
 	if sword.animation_player.is_animation_active():
 		await sword.animation_player.animation_finished
@@ -17,11 +17,17 @@ func enter() -> void:
 	combo_transition_timer.start(player.combo_transition_time)
 
 
-func physics_update(_delta : float) -> void:
-	if is_dashing:
-		player.position+= dash_dir*player.DASH_SPEED*player.dash_speed_mult
+func physics_update(delta : float) -> void:
+	var dir: Vector2 = Input.get_vector("left","right","up","down").normalized()
+	player.move(dir,delta)
+	if player.speed.length() >0:
+		animation_player.play("run")
 	else:
 		animation_player.play("idle")
+	#if is_dashing:
+		#player.position+= dash_dir*player.DASH_SPEED*player.dash_speed_mult
+	#else:
+		#animation_player.play("idle")
 
 func _on_combo_transition_timer_timeout() -> void:
 	transitioned.emit(self,"idle")
