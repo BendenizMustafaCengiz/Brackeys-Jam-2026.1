27c41f67f989e133d502981b68db734135e055ce
Muhammed Cengiz <muhammedcengiz5116@gmail.com>
2026-02-22 13:32:49 +0300

    dsadsa

--- Scripts/player_stats.gd
+++ Scripts/player_stats.gd
@@ -1,13 +1,13 @@
 class_name PlayerStats extends Node
 
 const init_damage := 20
-const init_max_health := 100
+const init_max_health := 150
 const init_max_speed := 600
 const init_knockback_mult := 500
 const init_can_dash = false
 
 var cur_damage := 20
-var cur_max_health := 100
+var cur_max_health := 150
 var cur_max_speed := 600
 var cur_knockback_mult := 1.0
 var cur_can_dash := false
